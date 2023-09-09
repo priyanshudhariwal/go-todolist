b03bbe0101afbfc635e2a593c0e9356322958a30
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-09-10 01:20:08 +0530

    added infinite loop to make app exit user controlled

--- todo-list.go
+++ todo-list.go
@@ -8,26 +8,28 @@ func main() {
 
 	var choice int
 
-	fmt.Println("Choose option:")
-	fmt.Println("1. View Todos")
-	fmt.Println("2. Add Todo")
-	fmt.Println("3. Delete Todo")
-	fmt.Println("4. Exit")
-	fmt.Println("Enter choice: ")
-	fmt.Scan(&choice)
-	fmt.Println(choice)
+	for {
+		fmt.Println("Choose option:")
+		fmt.Println("1. View Todos")
+		fmt.Println("2. Add Todo")
+		fmt.Println("3. Delete Todo")
+		fmt.Println("4. Exit")
+		fmt.Println("Enter choice: ")
+		fmt.Scan(&choice)
+		fmt.Println(choice)
 
-	switch choice {
-	case 1:
-		view()
-	case 2:
-		add()
-	case 3:
-		del()
-	case 4:
-		//
-	default:
-		fmt.Println("Enter correct option")
+		switch choice {
+		case 1:
+			view()
+		case 2:
+			add()
+		case 3:
+			del()
+		case 4:
+			//
+		default:
+			fmt.Println("Enter correct option")
+		}
 	}
 }
 
