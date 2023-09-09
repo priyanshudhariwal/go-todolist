e7e29ad88bd2b8e7c1683502836c4278a1604c67
Priyanshu Dhariwal <priyanshudhariwal@gmail.com>
2023-09-10 01:01:16 +0530

    testing changes and commits

--- todo-list.go
+++ todo-list.go
@@ -12,12 +12,33 @@ func main() {
 	fmt.Println("1. View Todos")
 	fmt.Println("2. Add Todo")
 	fmt.Println("3. Delete Todo")
+	fmt.Println("4. Exit")
 	fmt.Println("Enter choice: ")
 	fmt.Scan(&choice)
 	fmt.Println(choice)
 
 	switch choice {
 	case 1:
-
+		view()
+	case 2:
+		add()
+	case 3:
+		del()
+	case 4:
+		Exit
+	default:
+		fmt.Println("Enter correct option")
 	}
+}
+
+func view() {
+
+}
+
+func add() {
+
+}
+
+func del() {
+
 }
